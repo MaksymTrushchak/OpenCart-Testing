909070e1ebfc8fae816252fd1c3294d90a81eeb9
Artur Hurnytskyi <arturhurnytskyi@gmail.com>
2022-04-28 15:32:40 +0300

    Created empty cart test

---
+++ ShoppingCartEmpty.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace OpenCartClasses
+{
+    class ShoppingCartEmpty
+    {
+        static void Main(string[] args)
+        {
+            WebDriver driver = new ChromeDriver();
+            //driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+            driver.Navigate().GoToUrl("http://localhost/opencart/upload/");
+            driver.Manage().Window.Maximize();
+
+            driver.FindElement(By.Id("cart")).Click();
+
+            Thread.Sleep(2000);
+
+            driver.Quit();
+        }
+    }
+}
