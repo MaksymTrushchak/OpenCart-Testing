b058c5a595e530a72c7893a5679004c19976c1c3
Artur Hurnytskyi <arturhurnytskyi@gmail.com>
2022-04-28 20:04:09 +0300

    corrected namespaces

--- ShoppingCartEmpty.cs
+++ ShoppingCartEmpty.cs
@@ -1,9 +1,9 @@
 ﻿using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 
-namespace ShoppingCartEmpty
+namespace ShoppingCartFunctionals
 {
-    class ShoppingCartEmpty
+    class ShippingCartEmpty
     {
         static void Main()
         {
