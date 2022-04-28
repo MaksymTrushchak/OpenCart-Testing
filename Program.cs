f8bc22c1b200e6637f194882b33bd32d7ce7920b
Maksym <max.trushchak@gmail.com>
2022-04-28 15:33:15 +0300

    Initial

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Opencart
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
