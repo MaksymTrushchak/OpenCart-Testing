c7541e8fa70fb5d3560447444c0d6851b34b8497
Artur Hurnytskyi <arturhurnytskyi@gmail.com>
2022-04-28 20:04:23 +0300

    Refresh item class

---
+++ RefreshCartItem.cs
@@ -0,0 +1,19 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace ShoppingCartFunctionals
+{
+    class RefreshCartItem
+    {
+        static void Main()
+        {
+            WebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("http://localhost/opencart/upload/index.php?route=checkout/cart");
+            driver.Manage().Window.Maximize();
+
+            driver.FindElement(By.XPath("//*[@id='content']/form/div/table/tbody/tr[1]/td[4]/div/span/button[1]")).Click();
+
+            driver.Quit();
+        }
+    }
+}
