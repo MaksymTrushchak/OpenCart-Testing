7f7fb1c98601ccbe552cb6c8155297cf4654ff66
Artur Hurnytskyi <arturhurnytskyi@gmail.com>
2022-05-02 17:46:35 +0300

    extended RefreshItems method, added DeleteItems method

--- pages/ShoppingCart.cs
+++ pages/ShoppingCart.cs
@@ -1,4 +1,5 @@
-﻿using OpenQA.Selenium;
+﻿using System.Collections.Generic;
+using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 
 namespace OpencartTesting.pages
@@ -6,17 +7,40 @@ namespace OpencartTesting.pages
     class ShoppingCart
     {
         WebDriver driver;
-        IWebElement refresh;
+        IWebElement CartIsEmpty;
+        IWebElement RefreshItemButton;
+        IWebElement DeleteItemButton;
+        List<IWebElement> ProductNames;
 
         public ShoppingCart(WebDriver driver)
         {
             this.driver = driver;
-            refresh = driver.FindElement(By.XPath("//*[@id='content']/form/div/table/tbody/tr[1]/td[4]/div/span/button[1]"));
+            CartIsEmpty = driver.FindElement(By.CssSelector("//*[@id='content']/p"));
+            RefreshItemButton = driver.FindElement(By.XPath("//*[@id='content']/form/div/table/tbody/tr[1]/td[4]/div/span/button[1]"));
+            DeleteItemButton = driver.FindElement(By.XPath("//*[@id='content']/form/div/table/tbody/tr[1]/td[4]/div/span/button[2]"));
         }
 
-        public void RefreshItem()
+        public void RefreshItems(int items)
         {
-            refresh.Click();
+            for (int i = 0; i < items; i++)
+            {
+                RefreshItemButton.Click();
+                RefreshItemButton = driver.FindElement(By.XPath("//*[@id='content']/form/div/table/tbody/tr[1]/td[4]/div/span/button[1]" +
+                    "/following-sibling::button"));
+
+                if (RefreshItemButton == null) break;
+            }
+        }
+        public void DeleteItems(int items)
+        {
+            for (int i = 0; i < items; i++)
+            {
+                DeleteItemButton.Click();
+                DeleteItemButton = driver.FindElement(By.XPath("//*[@id='content']/form/div/table/tbody/tr[1]/td[4]/div/span/button[2]" +
+                    "/following-sibling::button"));
+
+                if (DeleteItemButton == null) break;
+            }
         }
     }
 }
