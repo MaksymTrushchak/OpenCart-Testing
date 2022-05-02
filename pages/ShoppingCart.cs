b7d054f82a8a44f8d2e61ea54fe1dd313bf967df
Artur Hurnytskyi <arturhurnytskyi@gmail.com>
2022-05-02 18:11:16 +0300

    added collapse methods and more objects, also getters

--- pages/ShoppingCart.cs
+++ pages/ShoppingCart.cs
@@ -7,10 +7,19 @@ namespace OpencartTesting.pages
     class ShoppingCart
     {
         WebDriver driver;
-        IWebElement CartIsEmpty;
-        IWebElement RefreshItemButton;
-        IWebElement DeleteItemButton;
+        private IWebElement CartIsEmpty;
         List<IWebElement> ProductNames;
+        private IWebElement RefreshItemButton;
+        private IWebElement DeleteItemButton;
+
+        private IWebElement CouponCodeExpandCollapse;
+        private IWebElement GiftCertExpandCollapse;
+
+        private IWebElement Subtotal { get; }
+        private IWebElement Total { get; }
+
+        private IWebElement ContinueShippingButton;
+        private IWebElement CheckoutButton;
 
         public ShoppingCart(WebDriver driver)
         {
@@ -18,6 +27,15 @@ namespace OpencartTesting.pages
             CartIsEmpty = driver.FindElement(By.CssSelector("//*[@id='content']/p"));
             RefreshItemButton = driver.FindElement(By.XPath("//*[@id='content']/form/div/table/tbody/tr[1]/td[4]/div/span/button[1]"));
             DeleteItemButton = driver.FindElement(By.XPath("//*[@id='content']/form/div/table/tbody/tr[1]/td[4]/div/span/button[2]"));
+
+            CouponCodeExpandCollapse = driver.FindElement(By.XPath("//*[@id='accordion']/div[1]/div[1]/h4/a"));
+            GiftCertExpandCollapse = driver.FindElement(By.XPath("//*[@id='accordion']/div[3]/div[1]/h4/a"));
+
+            Subtotal = driver.FindElement(By.XPath("//*[@id='content']/div[2]/div/table/tbody/tr[1]/td[2]"));
+            Total = driver.FindElement(By.XPath("//*[@id='content']/div[2]/div/table/tbody/tr[4]/td[2]"));
+
+            ContinueShippingButton = driver.FindElement(By.LinkText("Continue Shopping"));
+            CheckoutButton = driver.FindElement(By.LinkText("Checkout"));
         }
 
         public void RefreshItems(int items)
@@ -41,6 +59,14 @@ namespace OpencartTesting.pages
 
                 if (DeleteItemButton == null) break;
             }
+        }
+        public void ExColCouponCode()
+        {
+            CouponCodeExpandCollapse.Click();
+        }
+        public void ExColGiftCert()
+        {
+            GiftCertExpandCollapse.Click();
         }
     }
 }
