cbe4082c8e49103e54ed6dda7dbdcb37cf57b864
Artur Hurnytskyi <arturhurnytskyi@gmail.com>
2022-05-05 01:17:32 +0300

    pulled Maksym's TestRunner

--- Tools/TestRunner.cs
+++ Tools/TestRunner.cs
@@ -1,25 +1,19 @@
-﻿using NUnit.Framework;
-using OpencartTesting.Pages;
+﻿using System;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using NUnit.Framework;
 
 namespace OpencartTesting.Tools
 {
     public abstract class TestRunner
     {
         protected IWebDriver driver;
-
         protected abstract string OpenCartURL { get; }
         
         [SetUp]
         public void BeforeEachMethod()
         {
-            driver = new ChromeDriver(@"C:\Users\maxtr\OneDrive\Робочий стіл\SoftServe\Drivers");
+            driver = new ChromeDriver();
             driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(20);
             driver.Manage().Window.Maximize();
             driver.Navigate().GoToUrl(OpenCartURL);
@@ -30,14 +24,5 @@ namespace OpencartTesting.Tools
             driver.Close();
             driver.Quit();
         }
-
-     
-
-
-
     }
-
-
-
-
 }
