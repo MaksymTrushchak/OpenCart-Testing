247fd40d79e836e746c92c352936cf429a1ffb37
Artur Hurnytskyi <arturhurnytskyi@gmail.com>
2022-05-08 16:44:28 +0300

    added ProductPage

---
+++ Pages/ProductPage.cs
@@ -0,0 +1,16 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace OpencartTesting.Pages
+{
+    class ProductPage : MainPageHeader
+    {
+        public WebDriver driver;
+
+        public ProductPage(WebDriver driver) : base(driver)
+        {
+            this.driver = driver;
+
+        }
+    }
+}
