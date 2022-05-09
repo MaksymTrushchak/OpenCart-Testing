0dcd121357d6d83022809b4b9cc8ce5b7050cbd9
Artur Hurnytskyi <arturhurnytskyi@gmail.com>
2022-05-09 18:56:08 +0300

    transfered objects and methods to ShoppingCart

--- Pages/ProductsPage.cs
+++ Pages/ProductsPage.cs
@@ -7,53 +7,30 @@ namespace OpencartTesting.Pages
     class ProductsPage
     {
         public WebDriver driver;
-        private IWebElement product1;
-        private IWebElement product1Price;
-        private IWebElement product2;
-        private IWebElement product2Price;
-        private IWebElement product3;
-        private IWebElement product3Price;
-        private IWebElement addToCart;
-        private string addToCartXpath;
-        private IWebElement deleteFromCart;
-        private string deleteFromCartXpath;
-        private IWebElement cartButton;
-        private string cartButtonXpath;
-
+        private IWebElement Product1ToCart;
+        private IWebElement Product2ToCart;
+        private IWebElement Product3ToCart;
 
         public ProductsPage(WebDriver driver)
         {
             this.driver = driver;
 
-            product1 = driver.FindElement(By.XPath("//*[@id='content']/div[2]/div[1]"));
-            product1Price = driver.FindElement(By.ClassName("price"));
-
-            //addToCartXpath = "//*[@id='content']/div[2]/div[1]/div/div[2]/div[2]/button[1]";
-            addToCartXpath = "#content > div:nth-child(3) > div:nth-child(1) > div > div:nth-child(2) > div.button-group > button:nth-child(1)";
-            addToCart = driver.FindElement(By.CssSelector(addToCartXpath));
-
-            deleteFromCartXpath = "//*[@id='cart']/ul/li[1]/table/tbody/tr/td[5]/button";
-            deleteFromCart = driver.FindElement(By.XPath(deleteFromCartXpath));
-
-            cartButtonXpath = "//*[@id='cart']/button";
-            cartButton = driver.FindElement(By.XPath(cartButtonXpath));
+            Product1ToCart = driver.FindElement(By.XPath("//*[@id='content']/div[2]/div[1]/div/div[2]/div[2]/button[1]"));
+            Product2ToCart = driver.FindElement(By.XPath("//*[@id='content']/div[2]/div[2]/div/div[2]/div[2]/button[1]"));
+            Product3ToCart = driver.FindElement(By.XPath("//*[@id='content']/div[2]/div[3]/div/div[2]/div[2]/button[1]"));
 
         }
-        public IWebElement getAddToCartButton()
+        public void AddProduct1ToCart()
         {
-            return addToCart;
+            Product1ToCart.Click();
         }
-        public IWebElement getDeleteFromCartButton()
+        public void AddProduct2ToCart()
         {
-            return deleteFromCart;
+            Product2ToCart.Click();
         }
-        public IWebElement getCartButton()
+        public void AddProduct3ToCart()
         {
-            return cartButton;
-        }
-        public string ParseProductPrice()
-        {
-            return product1Price.Text;
+            Product3ToCart.Click();
         }
     }
 }
