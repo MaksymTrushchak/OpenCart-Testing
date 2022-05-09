6c695859cf453f86de7c16b453f061e3442110a6
Artur Hurnytskyi <arturhurnytskyi@gmail.com>
2022-05-09 22:35:15 +0300

    reworked test 1

--- Tests/ShoppingCartTest.cs
+++ Tests/ShoppingCartTest.cs
@@ -5,6 +5,7 @@ using OpencartTesting.Pages;
 using OpencartTesting.Tools;
 using NUnit.Framework;
 using System;
+using System.Threading;
 
 namespace OpencartTesting.tests
 {
@@ -23,42 +24,86 @@ namespace OpencartTesting.tests
             driver.Manage().Window.Maximize();
 
             DeleteFromCartTest(driver);
-            //GoToMainPage(driver);
-
-            //RefreshItemsTest(driver);
         }
 
         [Test]
         static void DeleteFromCartTest(WebDriver driver)
         {
+            string expectedResult = "Your shopping cart is empty!";
+            string actualResult;
+
             driver.FindElement(By.LinkText("Phones & PDAs")).Click();
 
             ProductsPage items = new ProductsPage(driver);
 
-            items.getAddToCartButton().Click();
+            items.AddProduct1ToCart();
+            // Only for Presentation
+            Thread.Sleep(500);
 
-            driver.Navigate().Refresh();
+            items.AddProduct2ToCart();
+            // Only for Presentation
+            Thread.Sleep(500);
+            
+            items.AddProduct3ToCart();
+            // Only for Presentation
+            Thread.Sleep(500);
 
-            items.getCartButton().Click();
-            items.getDeleteFromCartButton().Click();
+            driver.Navigate().GoToUrl("http://192.168.0.100/opencart/upload/index.php?route=checkout/cart");
+            ShoppingCart cart = new ShoppingCart(driver);
+            Thread.Sleep(500);
 
-            items.getCartButton().Click();
+            cart.RemoveProduct3();
+            cart.RemoveProduct2();
+            cart.RemoveProduct1();
 
-            string expectedResult = "Your shopping cart is empty!";
-            string actualResult = driver.FindElement(By.XPath("//*[@id='cart']/ul/li/p")).Text;
+            Thread.Sleep(1000);
 
+            actualResult = driver.FindElement(By.CssSelector("#content > p")).Text;
             Assert.AreEqual(expectedResult, actualResult);
+
+            // Only for Presentation
+            Thread.Sleep(1000);
+            cart.ClickContinue();
         }
 
         [Test]
         static void RefreshItemsTest(WebDriver driver)
         {
+            string expectedResult = "Your shopping cart is empty!";
+            string actualResult;
+
             driver.FindElement(By.LinkText("Phones & PDAs")).Click();
 
             ProductsPage items = new ProductsPage(driver);
 
-            items.getAddToCartButton().Click();
-            
+            items.AddProduct1ToCart();
+            // Only for Presentation
+            Thread.Sleep(500);
+
+            items.AddProduct2ToCart();
+            // Only for Presentation
+            Thread.Sleep(500);
+
+            items.AddProduct3ToCart();
+            // Only for Presentation
+            Thread.Sleep(500);
+
+            driver.Navigate().GoToUrl("http://192.168.0.100/opencart/upload/index.php?route=checkout/cart");
+            ShoppingCart cart = new ShoppingCart(driver);
+            Thread.Sleep(500);
+
+            cart.RemoveProduct3();
+            cart.RemoveProduct2();
+            cart.RemoveProduct1();
+
+            Thread.Sleep(1000);
+
+            actualResult = driver.FindElement(By.CssSelector("#content > p")).Text;
+            Assert.AreEqual(expectedResult, actualResult);
+
+            // Only for Presentation
+            Thread.Sleep(1000);
+            cart.ClickContinue();
         }
 
         static void GoToMainPage(WebDriver driver)
