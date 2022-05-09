999ce8e12a9c3b074ad28a451acbb2bea7099e3f
Artur Hurnytskyi <arturhurnytskyi@gmail.com>
2022-05-09 23:38:22 +0300

    minor changes

--- Pages/ShoppingCart.cs
+++ Pages/ShoppingCart.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections.Generic;
-using OpenQA.Selenium;
+﻿using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 
 namespace OpencartTesting.Pages
@@ -8,10 +7,12 @@ namespace OpencartTesting.Pages
     {
         public WebDriver driver;
 
-        private IWebElement DeleteProduct1;
-        private IWebElement DeleteProduct2;
-        private IWebElement DeleteProduct3;
+        private IWebElement RefreshProductButton;
+        private IWebElement DeleteProduct1Button;
+        private IWebElement DeleteProduct2Button;
+        private IWebElement DeleteProduct3Button;
 
+        private IWebElement Continue;
         private IWebElement Subtotal { get; }
         private IWebElement Total { get; }
 
@@ -20,27 +21,49 @@ namespace OpencartTesting.Pages
         public ShoppingCart(WebDriver driver)
         {
             this.driver = driver;
-           
-            DeleteProduct1 = driver.FindElement(By.XPath("//*[@id='content']/form/div/table/tbody/tr[3]/td[4]/div/span/button[2]"));
-            DeleteProduct2 = driver.FindElement(By.XPath("//*[@id='content']/form/div/table/tbody/tr[2]/td[4]/div/span/button[2]"));
-            DeleteProduct3 = driver.FindElement(By.XPath("//*[@id='content']/form/div/table/tbody/tr[1]/td[4]/div/span/button[2]"));
+
+            try
+            {
+                RefreshProductButton = driver.FindElement(By.XPath("//*[@id='content']/form/div/table/tbody/tr/td[4]/div/span/button[1]"));
+            } catch (OpenQA.Selenium.NoSuchElementException) { }
+
+            try
+            {
+                DeleteProduct1Button = driver.FindElement(By.XPath("//*[@id='content']/form/div/table/tbody/tr/td[4]/div/span/button[2]"));
+            }
+            catch (OpenQA.Selenium.NoSuchElementException) { }
+            
+            try
+            { 
+                DeleteProduct2Button = driver.FindElement(By.XPath("//*[@id='content']/form/div/table/tbody/tr[2]/td[4]/div/span/button[2]"));
+                DeleteProduct3Button = driver.FindElement(By.XPath("//*[@id='content']/form/div/table/tbody/tr[3]/td[4]/div/span/button[2]"));
+            }
+            catch (OpenQA.Selenium.NoSuchElementException) { }
 
             Subtotal = driver.FindElement(By.XPath("//*[@id='content']/div[2]/div/table/tbody/tr[1]/td[2]"));
             Total = driver.FindElement(By.XPath("//*[@id='content']/div[2]/div/table/tbody/tr[4]/td[2]"));
             CheckoutButton = driver.FindElement(By.LinkText("Checkout"));
         }
 
+        public void RefreshProduct()
+        {
+            RefreshProductButton.Click();
+        }
         public void RemoveProduct1()
         {
-            DeleteProduct1.Click();
+            driver.FindElement(By.XPath("//*[@id='content']/form/div/table/tbody/tr/td[4]/div/span/button[2]")).Click();
         }
         public void RemoveProduct2()
         {
-            DeleteProduct1.Click();
+            DeleteProduct2Button.Click();
         }
         public void RemoveProduct3()
         {
-            DeleteProduct1.Click();
+            DeleteProduct3Button.Click();
+        }
+        public void ClickContinue()
+        {
+            Continue = driver.FindElement(By.XPath("//*[@id='content']/div/div/a"));
         }
     }
 }
