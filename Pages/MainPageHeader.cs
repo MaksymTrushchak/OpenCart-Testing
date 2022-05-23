0ef53baa04a8afef9cfc15940cce8e8d324021e4
Artur Hurnytskyi <arturhurnytskyi@gmail.com>
2022-05-23 20:21:38 +0300

    reworked pageobject

--- Pages/MainPageHeader.cs
+++ Pages/MainPageHeader.cs
@@ -7,45 +7,43 @@ namespace OpencartTesting.Pages
     {
         protected WebDriver driver;
 
-        private IWebElement currency;
-        private IWebElement myAccount;
-        private IWebElement wishlist;
-        private IWebElement shoppingCart;
-        private IWebElement checkout;
+        public IWebElement shoppingCart
+        {
+            get
+            {
+                return driver.FindElement(By.XPath("//*[@id='top - links']/ul/li[4]"));
+            }
+        }
+        public IWebElement checkout
+        {
+            get
+            {
+                return driver.FindElement(By.XPath("//*[@id='top - links']/ul/li[5]"));
+            }
+        }
 
-        private IWebElement logo;
-        private IWebElement searchBar;
-        private IWebElement searchButton;
-        private IWebElement cartButton;
-        private IWebElement total;
-
-        private IWebElement topMenu;
+        public IWebElement cartButton
+        {
+            get
+            {
+                return driver.FindElement(By.XPath("//*[@id='cart']/button"));
+            }
+        }
+        private IWebElement total
+        {
+            get
+            {
+                return driver.FindElement(By.CssSelector("#cart-total"));
+            }
+        }
 
         public MainPageHeader(WebDriver driver)
         {
             this.driver = driver;
-
-            currency = driver.FindElement(By.XPath("//*[@id='form - currency']/div/button"));
-            myAccount = driver.FindElement(By.XPath("//*[@id='top - links']/ul/li[2]"));
-            wishlist = driver.FindElement(By.XPath("//*[@id='top - links']/ul/li[3]"));
-            shoppingCart = driver.FindElement(By.XPath("//*[@id='top - links']/ul/li[4]"));
-            checkout = driver.FindElement(By.XPath("//*[@id='top - links']/ul/li[5]"));
-
-            logo = driver.FindElement(By.CssSelector("#logo > a"));
-            searchBar = driver.FindElement(By.Name("search"));
-            searchButton = driver.FindElement(By.CssSelector(".btn.btn-default.btn-lg"));
-            cartButton = driver.FindElement(By.XPath("//*[@id='cart']/button"));
-            total = driver.FindElement(By.CssSelector("#cart-total"));
-
-            topMenu = driver.FindElement(By.CssSelector("//*[@id='menu']/div[2]/ul"));
         }
 
-        public IWebElement GetCurrency() => driver.FindElement(By.XPath("//*[@id='form-currency']/div/button/strong"));
         public void ClickShoppingCart() { shoppingCart.Click(); }
         public void ClickCheckout() { checkout.Click(); }
         public void ViewCart() { cartButton.Click(); }
-
-        public IWebElement getCartButton() => cartButton;
-        public IWebElement getTotal() => total;
     }
 }
